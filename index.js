cab63e8ef333440eeb2c749506e8553267a9285c
Sanjaee <afrizaahmad18@gmail.com>
2024-10-02 06:14:30 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const authRoutes = require("./src/routes/authRoutes")
+const barangRoutes = require('./src/routes/barangRoutes');
 
 const app = express()
 
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/auth", authRoutes)
+app.use("/api", barangRoutes)
 
 
 const port = process.env.PORT || 3000
